2448ac0ab6a7390fa412a5687a7704fed71ae963
xiaodongzhang <1907837146@qq.com>
2024-07-17 00:49:38 +0800

    feat: swagger codegen integration.

---
+++ sample/spring-swagger-codegen-integration/src/main/java/io/github/yaojiqunaer/Main.java
@@ -0,0 +1,13 @@
+package io.github.yaojiqunaer;
+
+/**
+ * @Title
+ * @Description: ${description}
+ * @Create Date: 2024/06/12 14:15
+ * @Author xiaodongzhang
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
