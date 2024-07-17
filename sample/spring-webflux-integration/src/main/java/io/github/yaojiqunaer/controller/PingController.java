3c583eeb861e2223897d534141d97ec49a9eafe4
xiaodongzhang <1907837146@qq.com>
2024-07-17 14:57:26 +0800

    feat: add webflux api endpoint demo.

--- sample/spring-webflux-integration/src/main/java/io/github/yaojiqunaer/controller/PingController.java
+++ sample/spring-webflux-integration/src/main/java/io/github/yaojiqunaer/controller/PingController.java
@@ -1,4 +1,23 @@
 package io.github.yaojiqunaer.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
 public class PingController {
+
+    @GetMapping("/ping")
+    public Mono<String> ping() {
+        return Mono.just("pong");
+    }
+
+    @GetMapping("/ping2")
+    public Flux<String> index() {
+        return Flux.fromIterable(List.of("pong", "pong2"));
+    }
+
 }
