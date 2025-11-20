61d6a8004adf50043722a9da3825dd93b604d3f8
xiaodongzhang <1907837146@qq.com>
2025-11-20 13:54:32 +0800

    feat: Add easy excel write demo.

--- sample/spring-easyexcel-integration/src/main/java/io/github/yaojiqunaer/Main.java
+++ sample/spring-easyexcel-integration/src/main/java/io/github/yaojiqunaer/Main.java
@@ -1,8 +1,11 @@
 package io.github.yaojiqunaer;
 
 import com.alibaba.excel.EasyExcel;
+import io.github.yaojiqunaer.entity.IndexOrNameData;
 import io.github.yaojiqunaer.entity.LargeData;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 @Slf4j
 public class Main {
@@ -11,8 +14,16 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         String fileName = "/Users/xiaodongzhang/IdeaProjects/github/java-technology-stack/sample/spring-easyexcel" +
-                "-integration/src/main/resources/large07.xlsx";
+                "-integration/src/main/resources/demo.xlsx";
         EasyExcel.read(fileName, LargeData.class, new LargeDataListener()).sheet(0).doRead();
+        // List 写入一个文件
+        IndexOrNameData indexOrNameData = new IndexOrNameData();
+        indexOrNameData.setName(DEFAULT_NAME);
+        indexOrNameData.setDoubleData(DEFAULT_NAME);
+        indexOrNameData.setString(DEFAULT_NAME);
+        EasyExcel.write("/Users/xiaodongzhang/IdeaProjects/github/java-technology-stack/sample/spring-easyexcel" +
+                        "-integration/src/main/resources/write.xlsx",
+                IndexOrNameData.class).sheet("模板").doWrite(List.of(indexOrNameData));
     }
 
 }
